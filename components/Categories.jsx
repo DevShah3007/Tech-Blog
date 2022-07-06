a5338f9701b4a0bfd33341f473d21ca59166944a
DevShah3007 <shahdevsandip@gmail.com>
2022-07-07 00:40:14 +0530

    first commit

---
+++ components/Categories.jsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
+
+import { getCategories } from '../services';
+
+const Categories = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories().then((newCategories) => {
+      setCategories(newCategories);
+    });
+  }, []);
+
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-8 pb-6 mb-4">
+      <h3 className="text-xl mb-4 font-semibold border-b pb-4">Categories</h3>
+      {categories.map((category, index) => (
+        <Link key={index} href={`/category/${category.slug}`}>
+          <span className={`cursor-pointer block ${(index === categories.length - 1) ? 'border-b-0' : 'border-b'} pb-2 mb-2`}>{category.name}</span>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default Categories;
